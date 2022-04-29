bc44194ef82b881a000e0ba74eb7fe2019dc1368
peterdudfield <peter.dudfield@hotmail.com>
2022-04-29 10:00:51 +0100

    add status on data consumer

--- application/tabs/status/layout.py
+++ application/tabs/status/layout.py
@@ -2,12 +2,49 @@
 import logging
 
 import dash_bootstrap_components as dbc
-from dash import dcc, html
+from dash import dcc, html, dash_table
+
+from .database import get_consumer_status
 
 logger = logging.getLogger(__name__)
 
 
 def make_status_layout():
-    tab3 = html.Div()
+    tab3 = html.Div(
+        [
+            html.H3("Data Consumers"),
+            dash_table.DataTable(
+                data=get_consumer_status(),
+                id="table-status",
+                style_data_conditional=[
+                    {
+                        'if': {
+                            'filter_query': '{Status} = "Warning"',
+                            'column_id': 'Status'
+                        },
+                        "backgroundColor": "orange",
+                        "color": "white",
+                    },
+                    {
+                        'if': {
+                            'filter_query': '{Status} = "Error"',
+                            'column_id': 'Status'
+                        },
+                        "backgroundColor": "red",
+                        "color": "white",
+                    },
+                    {
+                        'if': {
+                            'filter_query': '{Status} = "Ok"',
+                            'column_id': 'Status'
+                        },
+                        "backgroundColor": "green",
+                        "color": "white",
+                    },
+                ],
+            ),
+        ],
+        style={"width": "49%"},
+    )
 
     return tab3
