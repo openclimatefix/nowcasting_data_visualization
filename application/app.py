b42187c5bff0ed01b0484e67fc63e4e9e67854eb
BumpVersion Action <bumpversion@github-actions>
2022-05-05 13:51:51 +0000

    Bump version: 0.0.23 → 0.0.24

--- application/app.py
+++ application/app.py
@@ -20,7 +20,7 @@ logger = logging.getLogger(__name__)
 logger.setLevel(getattr(logging, os.environ.get("LOG_LEVEL", "DEBUG")))
 external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
 
-version = "0.0.23"
+version = "0.0.24"
 
 
 """Construct core Flask application with embedded Dash app."""
