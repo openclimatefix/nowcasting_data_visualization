129af6a98cfba73f968894b3eb8d18d85804271f
BumpVersion Action <bumpversion@github-actions>
2022-05-05 17:54:48 +0000

    Bump version: 0.0.26 → 0.0.27

--- application/app.py
+++ application/app.py
@@ -17,7 +17,7 @@ from tabs.summary.layout import make_layout
 # logger.setLevel(getattr(logging, os.environ.get("LOG_LEVEL", "DEBUG")))
 external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
 
-version = "0.0.26"
+version = "0.0.27"
 logger.debug(f"Running {version} of Data visulization ")
 print(version)
 
